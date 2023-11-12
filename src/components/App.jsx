faca88a353432f262a14e3bb2c2ddd65e4f087f1
martadelka <oamedvedeva777@gmail.com>
2023-11-12 19:34:36 +0200

    task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import user from '../data/user.json'
 import { Profile } from './Profile/Profile.jsx'
 
+import data from '../data/data.json'
+import { Statistics } from './Statistics/Statistics'
+
 export const App = () => {
   return (
     <div>
@@ -11,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import css from './Statistics.module.css'
+
+export const Statistics = ({ title, stats }) => {
+    return (
+        <section className={css.statistics}>
+            <h2 className={css.title}>{title}</h2>
+
+            <ul className={css.stats_list}>
+                {stats.map(({ id, label, percentage }) => {
+                    return (
+                        <li
+                            className={css.stats}
+                            key={id}
+                        >
+                            <span className={css.label}>{label}</span>
+                            <span className={css.percentage}>{percentage}%</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    )
+}
+
+
+
+
